ae1eeec768c940f029285177b4abdddc720b6217
thienJs <cristian.albarran95@gmail.com>
2019-03-10 14:36:48 -0400

    header component refactored to overlay

--- src/components/OverLay/OverLay.js
+++ src/components/OverLay/OverLay.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import classes from './Header.module.css';
+import classes from './OverLay.module.css';
+import { LeftArrow } from '../icons/Icons';
 
 export default () => (
   <div>
@@ -11,9 +13,9 @@ export default () => (
       <h2 className={classes.header__logan}>
         Donde los mejores eventos estan conectados a un click
       </h2>
-      <a href="/" className={classes.header__btn}>
-        EVENTIFY
-      </a>
+      <Link to="/app" className={classes.header__btn}>
+        EVENTIFY <LeftArrow w={17} h={17} fill="fill fill__white" />
+      </Link>
     </div>
   </div>
 );
