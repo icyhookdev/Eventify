4653cbfc27ce5f75bc2d75478f124d9f64476444
thienJs <cristian.albarran95@gmail.com>
2019-03-11 00:17:24 -0400

    app container refactored

---
+++ src/containers/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../../components/Nav/Nav';
+import classes from './App.module.css';
+
+export default () => {
+  return (
+    <div className={classes.App}>
+      <Nav />
+    </div>
+  );
+};
