25b92b79cffedf1cc3325da69339300252fee4ac
thienJs <cristian.albarran95@gmail.com>
2019-03-11 00:19:05 -0400

    fixing a bug

--- src/RouterLayout.js
+++ src/RouterLayout.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import OverLay from './components/OverLay/OverLay';
+import App from './containers/App/App';
 
 export default () => (
   <div>
     <Route path="/" exact component={OverLay} />
-    <Route path="/app" component={() => <h1>Appp</h1>} />
+    <Route path="/app" component={App} />
     <Route path="/login" component={OverLay} />
     <Route path="/register" component={OverLay} />
   </div>
