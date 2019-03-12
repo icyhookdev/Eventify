962e9f4c771d246ed6d90c432353fa8907ebfda0
thienJs <cristian.albarran95@gmail.com>
2019-03-12 00:36:50 -0400

    refactor routes

--- src/RouterLayout.js
+++ src/RouterLayout.js
@@ -3,10 +3,12 @@ import { Route } from 'react-router-dom';
 
 import OverLay from './components/OverLay/OverLay';
 import App from './containers/App/App';
+import Nav from './components/Nav/Nav';
 
 export default () => (
   <div>
-    <Route path="/" exact component={OverLay} />
+    <Nav />
+    <Route path="/" exact component={App} />
     <Route path="/app" component={App} />
     <Route path="/login" component={OverLay} />
     <Route path="/register" component={OverLay} />
