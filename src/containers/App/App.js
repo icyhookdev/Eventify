c609abe3e51720136de180e3df9420ce4490d665
thienJs <cristian.albarran95@gmail.com>
2019-03-13 02:45:33 -0400

    changing app compnent branding

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,13 +3,16 @@ import React from 'react';
 import classes from './App.module.css';
 import EventCard from '../../components/EventCard/EventCard';
 
-export default () => {
-  return (
-    <div className={classes.App}>
-      <div className={classes.events__container}>
-        <EventCard />
-      </div>
-      <div className={classes.side__content} />
+const App = () => (
+  <div className={classes.App}>
+    <div className={classes.events__container}>
+      <EventCard />
+      <EventCard />
     </div>
-  );
-};
+    <div className={classes.side__content}>
+      <h1>some content here</h1>
+    </div>
+  </div>
+);
+
+export default App;
