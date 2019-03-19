7fc0b452f03c2c4512a31100fbc5e68ceae5ab8e
thienJs <cristian.albarran95@gmail.com>
2019-03-19 14:30:31 -0400

    new icons added

---
+++ src/components/icons/AddIco.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Icons from './Icons';
+
+const AddIco = () => (
+  <Icons size="25" color="#fff">
+    <path d="M0,192v-192h192v192z" fill="none" />
+    <g>
+      <g id="surface1">
+        <path d="M92.16,34.56v57.6h-57.6v7.68h57.6v57.6h7.68v-57.6h57.6v-7.68h-57.6v-57.6z" />
+      </g>
+    </g>
+  </Icons>
+);
+
+export default AddIco;
