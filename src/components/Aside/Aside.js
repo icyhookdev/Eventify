84747a00aa9bf3441194c7efa75058cd2e45636f
thienJs <cristian.albarran95@gmail.com>
2019-03-23 12:07:19 -0400

    adding a basic aside layout

---
+++ src/components/Aside/Aside.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Aside.module.css';
+
+const Aside = () => (
+  <div className={classes.Aside}>
+    <div className={classes.search}>Busqueda Avanzada</div>
+  </div>
+);
+
+export default Aside;
