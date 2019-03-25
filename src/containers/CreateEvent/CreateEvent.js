a7778125c492fb1fe58f03d9af53055d9ebb6a73
thienJs <cristian.albarran95@gmail.com>
2019-03-25 00:37:33 -0400

    wire up create new event container with the view

--- src/containers/CreateEvent/CreateEvent.js
+++ src/containers/CreateEvent/CreateEvent.js
@@ -1,25 +1,11 @@
 import React from 'react';
 
-import useForm from '../../hooks/useForm';
 import classes from './CreateEvent.module.css';
-import InputGroup from '../../components/InputGroup/InputGroup';
+import NewEvent from '../../components/NewEvent/NewEvent';
 
-const CreateEvent = () => {
-  const { values, onChangeHandler } = useForm({ email: '' });
-  console.log(values);
-  return (
-    <div className={classes.CreateEvent}>
-      <h2>Crear Evento</h2>
-      <InputGroup
-        type="email"
-        name="email"
-        label="email"
-        errMsg=""
-        change={onChangeHandler}
-        value={values.email}
-      />
-    </div>
-  );
-};
-
+const CreateEvent = () => (
+  <div className={classes.CreateEvent}>
+    <NewEvent />
+  </div>
+);
 export default CreateEvent;
