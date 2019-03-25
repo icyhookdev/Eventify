3b4a38afd244c7d6c9bc6bf81f3ed64d6a3f2f6c
thienJs <cristian.albarran95@gmail.com>
2019-03-25 00:34:35 -0400

    refactor input component to work with dates and time

--- src/components/InputGroup/InputGroup.js
+++ src/components/InputGroup/InputGroup.js
@@ -3,6 +3,13 @@ import classes from './InputGroup.module.css';
 
 const InputGroup = ({ value, change, name, type, errMsg, label, touched }) => {
   const borderInput = errMsg ? 'err__input' : 'Input';
+  const objDate = new Date();
+
+  const today = `${objDate.getFullYear()}-${String(
+    objDate.getMonth() + 1
+  ).padStart(2, '0')}-${String(objDate.getDate()).padStart(2, '0')}`;
+  console.log(today, '2019-03-24');
+
   return (
     <div className={classes.group}>
       <input
@@ -14,8 +21,8 @@ const InputGroup = ({ value, change, name, type, errMsg, label, touched }) => {
         onChange={change}
         value={value}
         autoComplete="off"
-        min="1940-01-01"
-        max="2001-12-31"
+        min={today}
+        // max="2022-12-31"
       />
       <span className={classes.err__msg}>{errMsg}</span>
     </div>
