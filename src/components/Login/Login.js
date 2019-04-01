0ee232cf586120adf98b71d5bc317d234241e03f
thienJs <cristian.albarran95@gmail.com>
2019-04-01 12:56:08 -0400

    [FEATURE] validating inputs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,7 +6,7 @@ import OverLayBg from '../OverLayBg/OverLayBg';
 import loginBg from '../../assets/img/login.jpg';
 import InputGroup from '../InputGroup/InputGroup';
 
-const Login = () => (
+const Login = ({ values, change, submit, errors, touch, disable }) => (
   <div className={classes.Login}>
     <div className={classes.left}>
       <OverLayBg img={loginBg} />
@@ -16,14 +16,30 @@ const Login = () => (
     <div className={classes.right}>
       <h2>Iniciar Sesion</h2>
       <p>Inicia sesion para empezar a participar y crear eventos.</p>
-      <form className={classes.form}>
-        <InputGroup type="email" name="email" label="Email" />
-        <InputGroup type="password" name="password" label="Contrasena" />
+      <form className={classes.form} onSubmit={submit}>
+        <InputGroup
+          type="email"
+          name="email"
+          label="Email"
+          value={values.email}
+          change={change}
+          errMsg={errors.email}
+          touched={touch}
+        />
+        <InputGroup
+          type="password"
+          name="password"
+          label="Contrasena"
+          value={values.password}
+          change={change}
+          errMsg={errors.password}
+          touched={touch}
+        />
         <div className={classes.action}>
           <Link to="/register" className="auth auth__link">
             Registrase
           </Link>
-          <button type="button" className="auth auth__btn">
+          <button disabled={disable} type="submit" className="auth auth__btn">
             Iniciar sesion
           </button>
         </div>
