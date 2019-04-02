38bd441d55fb09afd2a86029fcc9f51d780b43fd
thienJs <cristian.albarran95@gmail.com>
2019-04-02 15:18:24 -0400

    [CHANGE] hooking up store in index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './store';
 import './index.css';
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.querySelector('#root')
 );
