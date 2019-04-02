c639fab96198338fcd3387cbb667fa6c91230b3d
thienJs <cristian.albarran95@gmail.com>
2019-04-02 15:14:01 -0400

    [CHANGE] minor change

--- src/components/InputGroup/InputGroup.js
+++ src/components/InputGroup/InputGroup.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import classes from './InputGroup.module.css';
 
-const InputGroup = ({ value, change, name, type, errMsg, label, touched }) => {
+const InputGroup = ({
+  value,
+  change,
+  name,
+  type,
+  errMsg,
+  label,
+  touched,
+  disable,
+}) => {
   const borderInput = errMsg ? 'err__input' : 'Input';
   const objDate = new Date();
 
   const today = `${objDate.getFullYear()}-${String(
     objDate.getMonth() + 1
   ).padStart(2, '0')}-${String(objDate.getDate()).padStart(2, '0')}`;
-
   return (
     <div className={classes.group}>
       <input
@@ -21,6 +29,7 @@ const InputGroup = ({ value, change, name, type, errMsg, label, touched }) => {
         value={value}
         autoComplete="off"
         min={today}
+        readOnly={disable}
       />
       <span className={classes.err__msg}>{errMsg}</span>
     </div>
