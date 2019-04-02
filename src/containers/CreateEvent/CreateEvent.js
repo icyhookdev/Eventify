e88743038c63ad6038d2285f38641e9c24ce5580
thienJs <cristian.albarran95@gmail.com>
2019-04-02 15:15:42 -0400

    [CHANGE] hooking up newEvent component

--- src/containers/CreateEvent/CreateEvent.js
+++ src/containers/CreateEvent/CreateEvent.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './CreateEvent.module.css';
 import NewEvent from '../../components/NewEvent/NewEvent';
 
-const CreateEvent = () => (
-  <div className={classes.CreateEvent}>
-    <NewEvent />
-  </div>
-);
+const CreateEvent = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  return (
+    <div className={classes.CreateEvent}>
+      <NewEvent
+        startDate={startDate}
+        endDate={endDate}
+        setDateS={setStartDate}
+        setDateE={setEndDate}
+      />
+    </div>
+  );
+};
 export default CreateEvent;
