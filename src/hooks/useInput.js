0683ac2bc624966cc959fc45c57ee28c56251117
thienJs <cristian.albarran95@gmail.com>
2019-04-02 15:16:31 -0400

    [FEATURE] a multi-use hook to handle inputs

---
+++ src/hooks/useInput.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const useInput = ({ preValues }) => {
+  const [values, setValues] = useState(preValues);
+
+  const onChangeHandler = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  return {
+    values,
+    onChangeHandler,
+  };
+};
+
+export default useInput;
