2d739981de3f46bb61103b569c9618b7386d6b89
thien <cristian.albarran95@gmail.com>
2019-04-14 01:15:12 -0400

    [REFACTOR] refatoring new event page

--- src/components/NewEvent/NewEvent.js
+++ src/components/NewEvent/NewEvent.js
@@ -1,80 +1,140 @@
 import React from 'react';
-import moment from 'moment';
+// import moment from 'moment';
+import 'react-datez/dist/css/react-datez.css';
+import { ReactDatez } from 'react-datez';
 
 import classes from './NewEvent.module.css';
 import Select from '../Select/Select';
 import InputGroup from '../InputGroup/InputGroup';
 import TextHeader from '../TextHeader/TextHeader';
 import SectionWrapper from './SectionWrapper/SectionWrapper';
-// import DatePicker from 'react-date-picker';
-import DatePickerInput from '../DatePicker/DatePickerInput';
-import Button from '../Button/Button';
+import BottomBar from '../BottomBar/BottomBar';
+import textIcon from '../../assets/icons/text.svg';
+import mapIcon from '../../assets/icons/map.svg';
+import dateIcon from '../../assets/icons/date.svg';
 
-const NewEvent = ({ startDate, endDate, setDateS, setDateE }) => (
+const NewEvent = ({
+  startDate,
+  endDate,
+  setDateS,
+  setDateE,
+  values,
+  change,
+  submit,
+  bbar,
+}) => (
   <div className={classes.NewEvent}>
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submit}>
       <TextHeader
         title="Informacion Basica"
         description="Dale nombre a tu evento y dile a los participantes por que deben asistir."
+        img={textIcon}
       />
-      <InputGroup label="Nombre del Evento" />
-      <div className={classes.event__group}>
-        <Select selected="Tipo" options={['presencial', 'online']} />
-        <Select selected="Categoria" options={['Musica', 'online']} />
-        <Select selected="Restriccion" options={['presencial', 'online']} />
-      </div>
-      <SectionWrapper>
-        <div className={classes.Location}>
-          <TextHeader
-            title="Lugar"
-            description="Ayuda a las personas en el area a descubrir tu evento y deja saber a los participantes donde esta ubicado."
+      <div className={classes.align__to_text}>
+        <InputGroup
+          name="name"
+          label="Nombre del Evento"
+          change={change}
+          value={values.name}
+        />
+        <div className={classes.event__group}>
+          <Select
+            name="type"
+            selected="Tipo"
+            options={['presencial', 'online']}
+            change={change}
+            value={values.type}
           />
           <Select
-            selected="Modalidad del evento"
+            name="category"
+            selected="Categoria"
+            options={['Musica', 'online']}
+            change={change}
+            value={values.category}
+          />
+          <Select
+            name="restriction"
+            selected="Restriccion"
             options={['presencial', 'online']}
+            change={change}
+            value={values.restriction}
           />
         </div>
-      </SectionWrapper>
+      </div>
       <SectionWrapper>
         <div className={classes.Date}>
           <TextHeader
             title="Fecha y Hora"
             description="Hasle saber a los participantes cuando empieza y termina el evento para que puedan asistir al mismo."
+            img={dateIcon}
           />
-          <div className={classes.Date__container}>
-            <div className={classes.Event__start}>
-              <h3 className={classes.semi_title}>Inicio del Evento</h3>
-              <InputGroup
-                type="text"
-                name="startDate"
-                disable
-                value={moment(startDate).format('L')}
-              />
-              <InputGroup type="time" name="startTime" />
-              <DatePickerInput date={startDate} change={setDateS} />;
-            </div>
-
-            <div className={classes.Event__ends}>
-              <h3 className={classes.semi_title}>Fin del Evento</h3>
-              <InputGroup
-                type="text"
-                name="endDate"
-                disable
-                value={moment(endDate).format('L')}
-              />
-              <InputGroup type="time" name="endTime" />
-              <DatePickerInput date={endDate} change={setDateE} />;
+          <div className={classes.align__to_text}>
+            <div className={classes.Date__container}>
+              <div className={classes.Event__start}>
+                <h3 className={classes.semi_title}>Inicio</h3>
+                <ReactDatez
+                  name="startDate"
+                  handleChange={setDateS}
+                  value={startDate}
+                />
+              </div>
+              <div className={classes.Event__ends}>
+                <h3 className={classes.semi_title}>Fin</h3>
+                <ReactDatez
+                  name="endDate"
+                  handleChange={setDateE}
+                  value={endDate}
+                />
+              </div>
             </div>
           </div>
         </div>
       </SectionWrapper>
-
-      <div className={classes.group}>
-        <div className={classes.group__btn}>
-          <Button type="button" buttonClass="cancel" text="Cancelar" />
-          <Button type="submit" buttonClass="action" text="Guardar" />
+      <SectionWrapper>
+        <div className={classes.Location}>
+          <TextHeader
+            title="Lugar"
+            description="Ayuda a las personas en el area a descubrir tu evento y deja saber a los participantes donde esta ubicado."
+            img={mapIcon}
+          />
+          <div className={classes.align__to_text}>
+            <Select
+              name="place"
+              selected="Modalidad del evento"
+              options={['presencial', 'online']}
+              change={change}
+              value={values.place}
+            />
+            {values.place === 'presencial' && (
+              <div className={classes.event__group}>
+                <Select
+                  name="country"
+                  selected="Modalidad del evento"
+                  options={['presencial', 'online']}
+                  change={change}
+                  value={values.country}
+                />
+                <Select
+                  name="state"
+                  selected="Modalidad del evento"
+                  options={['presencial', 'online']}
+                  change={change}
+                  value={values.state}
+                />
+                <Select
+                  name="city"
+                  selected="Modalidad del evento"
+                  options={['presencial', 'online']}
+                  change={change}
+                  value={values.city}
+                />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      </SectionWrapper>
+
+      {bbar && <BottomBar />}
     </form>
   </div>
 );
