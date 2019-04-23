3265e7c28f5531493a5bdf99d8f735f4335564bd
thien <cristian.albarran95@gmail.com>
2019-04-23 16:11:45 -0400

    creating an api for event and user authenticated

--- src/api/users.js
+++ src/api/users.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
-export default axios.create({
+export const users = axios.create({
   baseURL: 'https://iujo-eventify-api.herokuapp.com/',
 });
+
+export const usersWithAuth = token =>
+  axios.create({
+    baseURL: 'https://iujo-eventify-api.herokuapp.com/api/users',
+    headers: { Authorization: `Bearer ${token}` },
+  });
